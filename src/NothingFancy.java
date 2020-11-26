ca1b006e7be8593ccf25b3b6cb4e1a1a08716092
Computer <leonard.r.wecke@gmail.com>
2020-11-26 15:14:06 +0100

    Message added

--- src/NothingFancy.java
+++ src/NothingFancy.java
@@ -2,6 +2,7 @@ public class NothingFancy {
     public static void main(String[] args){
         System.out.println("i am the Master!");
         System.out.println("this was changed after branching Feature 2");
+        System.out.println("and this is Feature 1");
     }
 
 }
