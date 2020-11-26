843a124b664052a1e99b039856c96e55b44bd539
Computer <leonard.r.wecke@gmail.com>
2020-11-26 15:14:50 +0100

    Feature 2 added

--- src/NothingFancy.java
+++ src/NothingFancy.java
@@ -3,6 +3,7 @@ public class NothingFancy {
         System.out.println("i am the Master!");
         System.out.println("this was changed after branching Feature 2");
         System.out.println("and this is Feature 1");
+        System.out.println("and this is Feature 2");
     }
 
 }
